4629a8c43e64b12ae2899ff670c8ff11c86025cb
dovene <adovene@gmail.com>
2024-05-12 22:12:25 +0200

    enable actuator http trace

--- src/main/java/com/dev/loanmicroservice/LoanMicroserviceApplication.java
+++ src/main/java/com/dev/loanmicroservice/LoanMicroserviceApplication.java
@@ -1,13 +1,20 @@
 package com.dev.loanmicroservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LoanMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoanMicroserviceApplication.class, args);
+	}
+
+	@Bean
+	public InMemoryHttpExchangeRepository htttpTraceRepository() {
+		return new InMemoryHttpExchangeRepository();
 	}
 
 }
